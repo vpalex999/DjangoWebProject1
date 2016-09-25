2f8ca2cbfbc50cf98b3ff7a5a71efbad94bd21ae
vpalex999 <vpalex@mail.ru>
2016-09-25 20:27:27 +0500

    urls + views

--- DjangoWebProject1/DjangoWebProject1/urls.py
+++ DjangoWebProject1/DjangoWebProject1/urls.py
@@ -48,4 +48,5 @@ urlpatterns = [
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^iskratel/', include('iskratel.urls')),
+    url(r'^goods/$', include("page.urls")),
 ]
--- DjangoWebProject1/page/views.py
+++ DjangoWebProject1/page/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from page.models import *
 
-# Create your views here.
+def index(request):
+    cat_id=request.GET["id"]
+    if cat_id == None:
+        cat=Category.objects.first()
+    else:
+        cat=Category.objects.get(pk=cat_id)
+    
+    goods = Good.objects.filter(category=cat).order_by("name")
+    s = "Категория: "+cat.name+"<br><br>"
+    for good in goods:
+        s = s+"("+str(good.pk)+")"+good.name+"<br>"
+    return HttpResponse(s)
+
+def good(request):
+    return ""
